2cfcde09a3736c35eba95e03f610bdb45ae336ef
ivanbelosvetov <79517671+ivanbelosvetov@users.noreply.github.com>
2021-03-15 00:31:33 +0200

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let sample = [],
+    findIndex = 0,
+    sampleCounter = 0,
+    strCounter = 0;
+
+    for (let x = 0; x < bracketsConfig.length; x++) {
+        sample[x] = bracketsConfig[x].join('');
+    }
+   
+    while(sampleCounter < sample.length) {
+        while (strCounter < str.length) {
+            findIndex = str.indexOf(sample[sampleCounter]);
+            if (findIndex !== -1){
+                str = str.substring(0, findIndex) + str.substring(findIndex + sample[sampleCounter].length, str.length);
+                sampleCounter = 0;
+            } else {
+                strCounter = str.length;
+            }
+        }
+        sampleCounter++;
+        strCounter = 0;
+    }
+    return (!str);
 }
